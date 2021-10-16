ea09f16935b2b9d46c5e45b33a47340b1702ed87
levabala <levabala@ya.ru>
2021-10-16 22:47:30 +0300

    feat(Button): add css class forwarding

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,11 +1,15 @@
-import { Component, JSX } from "solid-js";
+import { cx } from "@emotion/css";
+import { Component, JSX, splitProps } from "solid-js";
 import styles from "./Button.module.scss";
 
 type ButtonProps = JSX.ButtonHTMLAttributes<HTMLButtonElement>;
 
 // TODO: add variants
 // TODO: add hover/active
-// TODO: add classes merging
-export const Button: Component<ButtonProps> = (props) => (
-  <button class={styles.Button} {...props} /> // children prop is passed as well
-);
+export const Button: Component<ButtonProps> = (props) => {
+  const [local, propsOther] = splitProps(props, ["class"]);
+
+  return (
+    <button {...propsOther} class={cx(styles.Button, local.class)} /> // children prop is passed as well
+  );
+};
