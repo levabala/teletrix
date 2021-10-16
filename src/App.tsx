42ce0eae1382b33ed6f0539801694e0447e7711d
levabala <levabala@ya.ru>
2021-10-16 14:54:13 +0300

    feat: base layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,23 @@
 import type { Component } from "solid-js";
 import styles from "./App.module.scss";
+import { Header, Button } from "./components";
+
+import menuIcon from "./assets/icons/menu.svg";
 
 const App: Component = () => {
-  return <main class={styles.App} />;
+  return (
+    <div class={styles.App}>
+      <Header
+        actionLeft={
+          <Button onClick={() => console.log("kek")}>
+            <img src={menuIcon} alt="menu icon" />
+          </Button>
+        }
+        content={<span>Header</span>}
+      />
+      <main class={styles.main} />
+    </div>
+  );
 };
 
 export default App;
