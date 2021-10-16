e27e1dcea56c3c5796f5d6568a400c0fe58351a2
levabala <levabala@ya.ru>
2021-10-16 12:21:08 +0300

    build: setup eslint

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
 import { defineConfig } from "vite";
 import solidPlugin from "vite-plugin-solid";
+import eslintPlugin from "vite-plugin-eslint";
 
 export default defineConfig({
-  plugins: [solidPlugin()],
+  plugins: [solidPlugin(), eslintPlugin()],
   build: {
     target: "esnext",
     polyfillDynamicImport: false,
